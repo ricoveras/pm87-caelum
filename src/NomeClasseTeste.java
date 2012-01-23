bbb5d505aec9456083281957191b67981050f3ab
Ricardo Veras <ricoveras@gmail.com>
2012-01-23 11:27:28 -0200

    primeiro commit do projeto

---
+++ src/NomeClasseTeste.java
@@ -0,0 +1,12 @@
+
+public class NomeClasseTeste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Exemplo de teste de uso do git");
+	}
+
+}
